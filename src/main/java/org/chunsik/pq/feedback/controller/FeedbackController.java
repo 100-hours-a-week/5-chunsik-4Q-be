080ef59a59ccbc69b7c794135de6676665855f7a
kimdaegeon <kdg870918@gmail.com>
2024-08-19 23:44:13 +0900

    refactor: 400,500 응답코드처리 구현
    PQ-73
    
    응답코드 처리 구현

--- src/main/java/org/chunsik/pq/feedback/controller/FeedbackController.java
+++ src/main/java/org/chunsik/pq/feedback/controller/FeedbackController.java
@@ -7,16 +7,26 @@ import org.chunsik.pq.feedback.service.FeedbackService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/feedback")
 public class FeedbackController {
+
     private final FeedbackService feedbackService;
 
     @PostMapping
     public ResponseEntity<Feedback> submitFeedback(@RequestBody FeedbackDTO feedbackDTO) {
-        Feedback feedback = feedbackService.saveFeedback(feedbackDTO);
-        return new ResponseEntity<>(feedback, HttpStatus.CREATED);
+        try {
+            Feedback feedback = feedbackService.saveFeedback(feedbackDTO);
+            return new ResponseEntity<>(feedback, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            // 400 Bad Request - 잘못된 입력 데이터가 있는 경우
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (Exception e) {
+            // 500 Internal Server Error - 서버에서 처리 중 오류가 발생한 경우
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred while processing the request.", e);
+        }
     }
 }
