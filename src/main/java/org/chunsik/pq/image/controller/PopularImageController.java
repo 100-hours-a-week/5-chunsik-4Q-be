2979a83b01a62c2b383eeb41646ed197b21664eb
kimdaegeon <kdg870918@gmail.com>
2024-08-19 15:57:31 +0900

    feat: 인기갤러리 기능 구현 완료
    PQ-33
    
    인기 갤러리 기능 구현 완료
    - 24시간 내 생성된 이미지의 image_path를 생성횟수로 내림차순 정렬해서 리스트로 응답
    - 24시간으로 필터링하기 위해 nativeQuery사용
    
    !! 엔티티 만들때 배경이미지와 태그_배경이미지 테이블 두개만 연결해놔서 다른 기능 개발하면서 테이블 연결할때 수정해야 합니다 !!

---
+++ src/main/java/org/chunsik/pq/image/controller/PopularImageController.java
@@ -0,0 +1,29 @@
+package org.chunsik.pq.image.controller;
+
+import lombok.RequiredArgsConstructor;
+
+import net.minidev.json.JSONUtil;
+import org.chunsik.pq.image.dto.ImageResponseDto;
+import org.chunsik.pq.image.service.ImageService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/images")
+public class PopularImageController {
+
+    private final ImageService service;
+
+    @GetMapping("/popular")
+    public List<ImageResponseDto> getTopPhotoBackgrounds() {
+        return service.getTopPhotoBackgrounds().stream()
+                .map(data -> new ImageResponseDto((String) data[0]))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/org/chunsik/pq/image/model/PhotoBackground.java
@@ -0,0 +1,27 @@
+package org.chunsik.pq.image.model;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "photo_background")
+public class PhotoBackground {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_path")
+    private String imagePath;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "size")
+    private Integer size;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+}
