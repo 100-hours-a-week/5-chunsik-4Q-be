91c3091e6670ebfeeb6d1376c776a3acccbdb698
HuttTheJAVA <chlghksdyd31@gmail.com>
2024-08-22 17:04:05 +0900

    fix: Karlo 서비스 종료로 인한 Karlo 관련 클래스 제거

--- src/main/java/org/chunsik/pq/generate/service/GenerateService.java
+++ src/main/java/org/chunsik/pq/generate/service/GenerateService.java
@@ -4,13 +4,11 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.chunsik.pq.generate.dto.GenerateImageDTO;
 import org.chunsik.pq.generate.dto.GenerateResponseDTO;
-import org.chunsik.pq.generate.manager.KarloManager;
 import org.chunsik.pq.generate.manager.OpenAIManager;
 import org.chunsik.pq.generate.model.BackgroundImage;
 import org.chunsik.pq.generate.repository.BackgroundImageRepository;
 import org.chunsik.pq.login.repository.UserRepository;
 import org.chunsik.pq.model.User;
-import org.chunsik.pq.s3.dto.S3UploadDTO;
 import org.chunsik.pq.s3.dto.S3UploadResponseDTO;
 import org.chunsik.pq.s3.manager.S3Manager;
 import org.chunsik.pq.s3.model.Ticket;
@@ -36,7 +34,6 @@ import java.util.UUID;
 public class GenerateService {
     private final S3Manager s3Manager;
     private final OpenAIManager openAIManager;
-    private final KarloManager karloManager;
     private final BackgroundImageRepository backgroundImageRepository;
     private final ShortenUrlRepository shortenURLRepository;
     private final UserRepository userRepository;
@@ -98,48 +95,6 @@ public class GenerateService {
         }
         Ticket ticket = new Ticket(user.get(), shortenURL.get(), backgroundImage, title, s3UploadResponseDTO.getS3Url());
         ticketRepository.save(ticket);
-    }
-
-    public GenerateResponseDTO generateImageKarlo(GenerateImageDTO generateImageDTO) throws IOException {
-        GenerateResponseDTO generateResponseDTO = karloManager.generateImage(generateImageDTO.getTags());
-
-        // URL에서 파일 이름만 추출
-        File jpgFile = downloadJpg(generateResponseDTO.getUrl());
-
-        // S3 업로드
-        S3UploadDTO s3UploadDTO = S3UploadDTO.builder()
-                .file(jpgFile)
-                .size(jpgFile.length())
-                .tags(generateImageDTO.getTags())
-                .userId(generateImageDTO.getUserId())
-                .categoryId(generateImageDTO.getCategoryId())
-                .build();
-
-        S3UploadResponseDTO s3UploadResponseDTO = s3Manager.uploadFile(s3UploadDTO.getFile(), generate);
-
-        // DB에 저장
-        BackgroundImage.BackgroundImageBuilder builder = BackgroundImage.builder()
-                .userId(s3UploadDTO.getUserId())
-                .url(s3UploadResponseDTO.getS3Url())
-                .categoryId(s3UploadDTO.getCategoryId())
-                .size(s3UploadDTO.getSize());
-
-        List<String> tags = s3UploadDTO.getTags();
-
-        switch (tags.size()) {
-            case 3:
-                builder.thirdTag(tags.get(2));
-            case 2:
-                builder.secondTag(tags.get(1));
-            case 1:
-                builder.firstTag(tags.get(0));
-        }
-
-        BackgroundImage backgroundImage = builder.build();
-
-        backgroundImageRepository.save(backgroundImage);
-
-        return new GenerateResponseDTO(backgroundImage.getUrl());
     }
 
     // url 이미지를 File로 매핑해 리턴하는 메소드
