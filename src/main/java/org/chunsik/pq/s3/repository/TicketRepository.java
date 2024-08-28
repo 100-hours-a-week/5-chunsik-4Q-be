b46fb86be8b72f9c81a2688017ee2889bd5d9dfb
kimdaegeon <kdg870918@gmail.com>
2024-08-28 18:26:24 +0900

    feat: 나의 4Q 태그 검색, 제목 검색 API 구현
    PQ-27 > PQ-53 > PQ-116
    
    나의 4Q 태그 검색, 제목 검색 API 구현
    - 공통 로직 발생으로 중복된 코드 메서드로 처리

--- src/main/java/org/chunsik/pq/s3/repository/TicketRepository.java
+++ src/main/java/org/chunsik/pq/s3/repository/TicketRepository.java
@@ -9,22 +9,20 @@ import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    // userId로 티켓을 필터링하고 생성일자 내림차순으로 정렬하는 메서드
+    // userId로 티켓을 필터링하고 생성일자 내림차순으로 정렬하면서 BackgroundImage를 fetch join
     @Query("SELECT t FROM Ticket t JOIN FETCH t.backgroundImage b WHERE t.userId = :userId ORDER BY t.createdAt DESC")
     List<Ticket> findTicketsByUserIdWithBackgroundImageOrderByCreatedAtDesc(@Param("userId") Long userId);
 
-    // userId로 티켓을 필터링하고 제목을 포함하는 티켓을 생성일자 내림차순으로 정렬하는 메서드
-    @Query("SELECT t FROM Ticket t WHERE t.userId = :userId AND t.title LIKE %:title% ORDER BY t.createdAt DESC")
+    // userId로 티켓을 필터링하고 제목을 포함하는 티켓을 생성일자 내림차순으로 정렬하면서 BackgroundImage를 fetch join
+    @Query("SELECT t FROM Ticket t JOIN FETCH t.backgroundImage b WHERE t.userId = :userId AND t.title LIKE %:title% ORDER BY t.createdAt DESC")
     List<Ticket> findTicketsByUserIdAndTitleContainingOrderByCreatedAtDesc(@Param("userId") Long userId, @Param("title") String title);
 
-    // 태그로 필터링된 티켓을 조회하는 메서드
+    // 태그로 필터링된 티켓을 조회하면서 BackgroundImage와 TagBackgroundImage를 fetch join
     @Query("SELECT t FROM Ticket t " +
-            "JOIN TagBackgroundImage tbi ON t.backgroundImage.id = tbi.photoBackgroundId " +
-            "JOIN Tag tag ON tbi.tagId = tag.id " +
+            "JOIN FETCH t.backgroundImage b " +
+            "JOIN FETCH TagBackgroundImage tbi ON b.id = tbi.photoBackgroundId " +
+            "JOIN FETCH Tag tag ON tbi.tagId = tag.id " +
             "WHERE t.userId = :userId AND tag.name = :tag " +
             "ORDER BY t.createdAt DESC")
     List<Ticket> findTicketsByUserIdAndTagNameOrderByCreatedAtDesc(@Param("userId") Long userId, @Param("tag") String tag);
-
-
 }
-
