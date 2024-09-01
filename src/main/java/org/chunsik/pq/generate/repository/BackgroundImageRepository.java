84dcf153dd322d007c5868f8de5006bc39b29074
HuttTheJAVA <chlghksdyd31@gmail.com>
2024-09-02 01:00:35 +0900

    feat: 연관 이미지 추천 기능 구현 - 태그 기준으로 상세 이미지와 연관된 이미지를 추천

--- src/main/java/org/chunsik/pq/generate/repository/BackgroundImageRepository.java
+++ src/main/java/org/chunsik/pq/generate/repository/BackgroundImageRepository.java
@@ -1,7 +1,34 @@
 package org.chunsik.pq.generate.repository;
 
+import jakarta.persistence.Tuple;
 import org.chunsik.pq.generate.model.BackgroundImage;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BackgroundImageRepository extends JpaRepository<BackgroundImage, Long> {
+    @Query(value = """
+            SELECT bi.id, bi.url
+            FROM (
+                SELECT bi.id, bi.url, COUNT(*) AS cnt
+                FROM background_image bi
+                JOIN tag_background_image tbi
+                ON bi.id = tbi.photo_background_id
+                GROUP BY bi.id
+            ) bi
+            JOIN (
+                SELECT *
+                FROM tag_background_image tbi
+                WHERE tbi.tag_id IN (:tagIds)
+            ) tbi
+            ON bi.id = tbi.photo_background_id
+            WHERE bi.id != :backgroundImgId
+            GROUP BY bi.id
+            ORDER BY COUNT(DISTINCT tbi.tag_id) DESC, bi.cnt ASC
+            """, nativeQuery = true)
+    Slice<Tuple> findRelateImgByTags(@Param("tagIds") List<Long> tagIds, Pageable pageable, Long backgroundImgId);
 }
