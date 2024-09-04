3b9cdafca4d60e76a8968148a58b8bd9dd91a94e
kimdaegeon <kdg870918@gmail.com>
2024-09-04 12:57:20 +0900

    feat: 나의 4Q 리스트 응답에 태그명, 배경이미지 좋아요 개수 정보 추가
    PQ-27 - PQ-53 - PQ-115
    
    나의 4Q 리스트 응답에 태그명, 배경이미지 좋아요 개수 정보 추가
    - 태그명과 배경이미지 매핑해서 쿼리수 감소
    - MyPageController 일반예외핸들러 삭제
    - 엔드포인트 myPQ -> mypq 소문자로 변경

--- src/main/java/org/chunsik/pq/gallery/repository/UserLikeRepository.java
+++ src/main/java/org/chunsik/pq/gallery/repository/UserLikeRepository.java
@@ -8,9 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserLikeRepository extends JpaRepository<UserLike, Long> {
-    Optional<UserLike> findByUserIdAndPhotoBackgroundId(Long userId, Long photoBackgroundId);
-
-    boolean existsByUserIdAndPhotoBackgroundId(Long userId, Long imageId);
 
     void deleteByUserIdAndPhotoBackgroundId(Long userId, Long imageId);
+
 }
--- src/main/java/org/chunsik/pq/generate/repository/TagRepository.java
+++ src/main/java/org/chunsik/pq/generate/repository/TagRepository.java
@@ -2,9 +2,15 @@ package org.chunsik.pq.generate.repository;
 
 import org.chunsik.pq.generate.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Optional<Tag> findByName(String name);
+
+    @Query("SELECT tg.name, tbi.photoBackgroundId FROM Tag tg " +
+            "JOIN TagBackgroundImage tbi ON tbi.tagId = tg.id")
+    List<Object[]> findAllTagsWithBackgroundIds();
 }
