c2d3c74d31cb6a2031256c99f58f916aaeee994c
HeungY <rkrkrkrk02@gmail.com>
2024-09-05 15:24:37 +0900

    fix: 회원 정보 수정 오류 수정
    PQ-129
    
    ModifyRequestDTO
    - 기본 생성자 추가해서 기존 초기화 안되던 오류 수정했습니다.

--- src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
+++ src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
@@ -4,14 +4,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class ModifyRequestDTO {
 
     @NotNull
     @NotBlank
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,16}$", message = "닉네임은 특수문자를 제외한 2~16자리여야 합니다.")
-    private final String nickname;
+    private String nickname;
 }
