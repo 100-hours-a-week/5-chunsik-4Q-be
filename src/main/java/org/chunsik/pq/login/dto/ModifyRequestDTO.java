407fbc2b7f70c755174a799fba65cbdc1510f42d
HeungY <rkrkrkrk02@gmail.com>
2024-09-05 15:03:32 +0900

    feat: 회원정보 수정 구현
    PQ-130
    
    UserController
    - /users/modify 엔드포인트로 nickname PATCH 요청
    UserService
    - modify() 객체
    - 현재 유저의 정보를 받아와서 이메일을 통해 DB 정보 반환.
    - nickname 수정 후 DB에 덮어씀

--- src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
+++ src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
@@ -4,14 +4,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class ModifyRequestDTO {
 
     @NotNull
     @NotBlank
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,16}$", message = "닉네임은 특수문자를 제외한 2~16자리여야 합니다.")
-    private String nickname;
+
+    private final String nickname;
 }
