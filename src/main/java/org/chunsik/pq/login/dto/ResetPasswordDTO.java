2bafbf9403f37f5992d38e6b1d19dad776de66a9
HeungY <rkrkrkrk02@gmail.com>
2024-09-05 13:51:10 +0900

    feat: 비밀번호 재설정 구현
    PQ-130
    
    EmailController
    - /users/email/update
    -> 이메일 인증 후 DB에 해당하는 이메일이 있을시, 정상 응답
    
    UserController
    - /users/reset
    -> 비밀번호 재설정.
    
    /users/email/update로 이메일 확인 하고, /users/reset 으로 이메일과 새로운 비밀번호 입력하면 비밀번호 재설정 완료됩니다.

---
+++ src/main/java/org/chunsik/pq/login/dto/ResetPasswordDTO.java
@@ -0,0 +1,18 @@
+package org.chunsik.pq.login.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ResetPasswordDTO {
+    private final String email;
+
+    @NotNull
+    @NotBlank(message="비밀번호는 필수 입력값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    private final String password;
+}
