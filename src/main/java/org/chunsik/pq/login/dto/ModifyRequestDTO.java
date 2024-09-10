cc0af9008bd251b2546b84d122b818bf3d314e07
HeungY <rkrkrkrk02@gmail.com>
2024-09-10 17:33:52 +0900

    fix: EmailController 엔드포인트 분리
    PQ-130
    EmailController
    - 하나의 엔드포인트에서 두개의 로직 수행중이던 코드 변경
    -> /users/email/verification
    -> /users/email/reset
    두개의 엔드포인트로 분리
    ModifyRequestDTO
    - A-Z 추가

--- src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
+++ src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 public class ModifyRequestDTO {
 
     @NotBlank
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,16}$", message = "닉네임은 특수문자를 제외한 2~16자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,16}$", message = "닉네임은 특수문자를 제외한 2~16자리여야 합니다.")
     private String nickname;
 }
