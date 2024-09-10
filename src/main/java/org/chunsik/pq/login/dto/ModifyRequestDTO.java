fd7f0499e7e3e14067e98da0ea7206734a9a63da
HeungY <rkrkrkrk02@gmail.com>
2024-09-10 15:17:40 +0900

    fix: null 체크 중복 제거
    PQ-130
    
    ModifyRequestDTO
    - @NotBlank가 NotNull의 역할도 하기때문에 @NotNull 제거

--- src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
+++ src/main/java/org/chunsik/pq/login/dto/ModifyRequestDTO.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ModifyRequestDTO {
 
-    @NotNull
     @NotBlank
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,16}$", message = "닉네임은 특수문자를 제외한 2~16자리여야 합니다.")
     private String nickname;
