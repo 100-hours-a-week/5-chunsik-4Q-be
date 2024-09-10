0005ff38e3a96d5faabf0a60f6e9e716cfca55ba
ZZ3n <ndsrhkd@naver.com>
2024-09-10 16:33:34 +0900

    feat: add login expiration time
    PQ-132
    
    - login response 수정
    - login service DTO 수정

--- src/main/java/org/chunsik/pq/login/controller/AuthController.java
+++ src/main/java/org/chunsik/pq/login/controller/AuthController.java
@@ -3,27 +3,24 @@ package org.chunsik.pq.login.controller;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.chunsik.pq.login.dto.AccessTokenResponseDto;
+import org.chunsik.pq.login.dto.AccessTokenWithExpirationDto;
 import org.chunsik.pq.login.exception.JwtValidationExpireException;
 import org.chunsik.pq.login.security.JwtTokenProvider;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     private final JwtTokenProvider jwtTokenProvider;
-    private static final String ACCESS_TOKEN = "accessToken";
     private static final String REFRESH_TOKEN = "refreshToken";
 
     @PostMapping("/refresh")
-    public ResponseEntity<?> refreshAccessToken(HttpServletRequest request) {
+    public ResponseEntity<AccessTokenResponseDto> refreshAccessToken(HttpServletRequest request) {
 
         String refreshToken = getRefreshTokenFromCookies(request);
         try {
@@ -31,10 +28,9 @@ public class AuthController {
                 throw new JwtValidationExpireException();
             } else {
                 String email = jwtTokenProvider.getMemberEmail(refreshToken);
-                String newAccessToken = jwtTokenProvider.createToken(email);
-                Map<String, String> response = new HashMap<>();
-                response.put(ACCESS_TOKEN, newAccessToken);
-                return new ResponseEntity<>(response, HttpStatus.OK);
+                AccessTokenWithExpirationDto accessTokenDto = jwtTokenProvider.createToken(email);
+
+                return ResponseEntity.ok(new AccessTokenResponseDto(accessTokenDto.getAccessToken(), accessTokenDto.getExpiration()));
             }
         } catch (Exception e) { // 재발급 로직 수행 중 예외 발생 시 재로그인 요구 예외를 던진다.
             throw new JwtValidationExpireException();
