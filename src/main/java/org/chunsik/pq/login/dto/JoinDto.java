eab0ce33d0b6b8d4c4a95e5b59598f8305df8f09
kimdaegeon0918 <105616992+kimdaegeon0918@users.noreply.github.com>
2024-09-10 17:30:16 +0900

    chore: NotBlank 두번 사용한것 삭제

--- src/main/java/org/chunsik/pq/login/dto/JoinDto.java
+++ src/main/java/org/chunsik/pq/login/dto/JoinDto.java
@@ -18,7 +18,6 @@ public class JoinDto {
     @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$")
     private String email;
 
-    @NotBlank
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
