5097fdeeac398d7b66840e9a435c49a9b9cab5a4
kimdaegeon <kdg870918@gmail.com>
2024-10-02 11:05:25 +0900

    feat: AOP로 실행시간 측정, prometheus로 수집
    - service 패키지에만 적용해서 속도 측정
    - /mypq/liked api가 controller에서 바로 repository를 실행하는 부분을 service를 거쳐가는 로직으로 변경
    - build.gradle 의존성 추가

--- src/main/java/org/chunsik/pq/generate/service/BackgroundImageService.java
+++ src/main/java/org/chunsik/pq/generate/service/BackgroundImageService.java
@@ -4,17 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.chunsik.pq.gallery.dto.BackgroundImageDTO;
 import org.chunsik.pq.gallery.model.GallerySort;
 import org.chunsik.pq.generate.repository.BackgroundImageRepository;
+import org.chunsik.pq.generate.repository.BackgroundImageRepositoryImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class BackgroundImageService {
 
     private final BackgroundImageRepository backgroundImageRepository;
-
+    private final BackgroundImageRepositoryImpl backgroundImageRepositoryimpl;
     public Page<BackgroundImageDTO> getBackgroundImages(String tagName, String categoryName, GallerySort sort, Pageable pageable) {
         return backgroundImageRepository.findByTagAndCategory(tagName, categoryName, sort, pageable);
+    }
+
+    public List<BackgroundImageDTO> getLikedBackgroundImages() {
+        return backgroundImageRepositoryimpl.findLikedBackgroundImagesWithoutPagination();
     }
 }
